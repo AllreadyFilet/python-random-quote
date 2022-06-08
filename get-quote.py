901de8f51d6415e0b32ccedf716b29241380db7c
AllreadyFilet <86827896+AllreadyFilet@users.noreply.github.com>
2022-06-08 13:14:32 +0500

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,23 @@
 def primary():
 
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = f.readlines(Responsive is better than fast
+It’s not fully shipped until it’s fast
+Anything added dilutes everything else
+Practicality beats purity
+Approachable is better than simple
+Mind your words, they are important
+Speak like a human
+Half measures are as bad as nothing at all
+Encourage flow
+Non-blocking is better than blocking
+Favor focus over features
+Avoid administrative distraction
+Design for failure
+Keep it logically awesome)
   f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   primary()
